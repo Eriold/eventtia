54f03d374d753eaa9108d8923e952b370bf1f21a
Daniel DM. Montoya <dmontoya@NET2.NET2>
2020-08-08 14:50:39 -0500

    Add correctly router Error

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-// import { BoardEvent, BoardRegisted } from 'components/Board'
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import { Step1, Step2, Step3, Step4 } from 'pages/Steps'
 import { Error } from 'pages/Error'
 
 const Routes = () => {
   return (
-    <>
+    <Switch>
       <Route exact path='/' component={Step1} />
       <Route exact path='/Step2' component={Step2} />
       <Route exact path='/Step3' component={Step3} />
       <Route exact path='/Step4' component={Step4} />
       <Route exact path='/null' render={() => (<Redirect to='/' />)} />
       <Route component={Error} />
-    </>
+    </Switch>
   )
 }
 
@@ -21,8 +20,6 @@ export const App = () => {
   return (
     <Router>
       <Routes />
-      {/* <BoardEvent title='Live Event Analytics' liveShow />
-      <BoardRegisted /> */}
     </Router>
   )
 }
